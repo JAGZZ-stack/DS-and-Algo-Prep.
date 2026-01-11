634be8c9edeb7e7fc43d679cd2b11e53366cd6e0
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-11 17:03:45 +0530

    Update 1.2 Contains Duplicates.java

--- 1 Hashmaps and Hashsets/1.2 Contains Duplicates.java
+++ 1 Hashmaps and Hashsets/1.2 Contains Duplicates.java
@@ -1,5 +1,10 @@
 // Contains Duplicates : https://leetcode.com/problems/contains-duplicate/
 
+// Brute Force : 
+// For each element, check if the duplicate exists.
+// TC : O(n * n), SC : O(1)
+
+// HashSet Based Approach
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
