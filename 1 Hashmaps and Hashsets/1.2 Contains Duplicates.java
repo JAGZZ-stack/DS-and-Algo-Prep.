488e7d390d12efb7924bd340df3dbadb7e413ddf
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-11 17:01:03 +0530

    Update 1.2 Contains Duplicates.java

--- 1 Hashmaps and Hashsets/1.2 Contains Duplicates.java
+++ 1 Hashmaps and Hashsets/1.2 Contains Duplicates.java
@@ -1,4 +1,5 @@
 // Contains Duplicates : https://leetcode.com/problems/contains-duplicate/
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
