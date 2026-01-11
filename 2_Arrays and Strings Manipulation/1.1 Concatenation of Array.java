4b066a690cde039bc19bf867934bfd01016744a6
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-11 16:41:25 +0530

    Update 1.1 Concatenation of Array.java

--- 2_Arrays and Strings Manipulation/1.1 Concatenation of Array.java
+++ 2_Arrays and Strings Manipulation/1.1 Concatenation of Array.java
@@ -1,1 +1,17 @@
+// Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/
 
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[i + nums.length] = nums[i];
+        }
+
+        return result;
+    }
+}
+
+// TC : O(n)
+// SC : O(n)
