835fba466ba1bbf2641b0e4334179e1f574cfc9b
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-13 23:42:16 +0530

    Update 0_Essential Collections and Functions.java

--- 0_Essential Collections and Functions.java
+++ 0_Essential Collections and Functions.java
@@ -1,2 +1,6 @@
 // Declare the matrix globally and assign the values inside any function
-private int[][] prefixSum;
+private int[][] prefixSum; // Global Declaration.
+
+void solve() {
+  this.prefixSum = new int[rows][columns];
+} // Initialise inside the function separately
