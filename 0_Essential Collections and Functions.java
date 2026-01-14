2acc48ad462a3bf9f9f2acf8c3c246cee2e098f2
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-14 10:46:42 +0530

    Update 0_Essential Collections and Functions.java

--- 0_Essential Collections and Functions.java
+++ 0_Essential Collections and Functions.java
@@ -4,3 +4,8 @@ private int[][] prefixSum; // Global Declaration.
 void solve() {
   this.prefixSum = new int[rows][columns];
 } // Initialise inside the function separately
+
+
+List of Problems to revise one week before the interview :
+1. Sort Colors
+2. Moore Voting Algorithm (Majority Element 1 and Majority Element 2)
