f74ffc374b30219753612eea672dff96ae4bfb46
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-14 20:58:34 +0530

    Update 1.1 Concatenation of Array.java

--- 2_Arrays and Strings Manipulation/1.1 Concatenation of Array.java
+++ 2_Arrays and Strings Manipulation/1.1 Concatenation of Array.java
@@ -2,6 +2,8 @@
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
+        if(nums.length == 0) return nums;
+
         int[] result = new int[nums.length * 2];
 
         for(int i = 0; i < nums.length; i++) {
