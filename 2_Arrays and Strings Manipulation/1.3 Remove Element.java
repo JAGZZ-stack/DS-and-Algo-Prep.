c0f5ee3a6ab9fa1a8c6f8a1ea387e2925d3a3637
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-14 21:15:04 +0530

    Update 1.3 Remove Element.java

--- 2_Arrays and Strings Manipulation/1.3 Remove Element.java
+++ 2_Arrays and Strings Manipulation/1.3 Remove Element.java
@@ -1,18 +1,51 @@
 // Remove Element - https://leetcode.com/problems/remove-element/submissions/1881895023/
 
+// Brute Force :
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0; // Represents the index where the next unique element needs to be placed.
+        if(nums.length == 0) return 0;
 
-        for(int j = 0; j < nums.length; j++) {
-            if(nums[j] != val) {
-                nums[i] = nums[j];
-                i++; // Important setup for Slow and Fast Pointers 
-            }
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+
+        for(int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        return i; // Understand the reasoning behind such setups in fast and slow pointers
+
+        int j = 0;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getKey() != val) {
+                for(int i = 0; i < entry.getValue(); i++) {
+                    nums[j] = entry.getKey();
+                    j++;
+                }
+            }
+            
+        }
+
+        return j;
     }
 }
 
 // TC : O(n)
+// SC : O(n)
+
+// Optimised Approach : 
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        
+        int i = 0; // Index at which the next non-val element should be placed
+
+        for(int j = 0; j < nums.length; j++) {
+            if(nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
+
+// TC : O(n) 
 // SC : O(1)
