ed79936cb72bea8b64bef4e53ab864d385d0cbaa
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-14 20:55:50 +0530

    Update 1.2 Remove Duplicates from Sorted Array.java

--- 2_Arrays and Strings Manipulation/1.2 Remove Duplicates from Sorted Array.java
+++ 2_Arrays and Strings Manipulation/1.2 Remove Duplicates from Sorted Array.java
@@ -1,47 +1,50 @@
 // Remove Duplicates from Sorted Array - https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-// HashSet Based Approach
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new LinkedHashSet<>();
+// Brute Force Approach : 
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         HashSet<Integer> set = new LinkedHashSet<>(); // To preserve the orders (LinkedHashSet)
 
-        for(int val : nums) {
-            if(!set.contains(val)) {
-                set.add(val);
-            }
-        }
+//         for(int val : nums) {
+//             if(!set.contains(val)) {
+//                 set.add(val);
+//             }
+//         }
 
-        int index = 0;
-        for(int val : set) {
-            nums[index] = val;
-            index++;
-        }
+//         int i = 0;
+//         for(int val : set) {
+//             nums[i] = val;
+//             i++;
+//         }
 
-        return index;
-    }
-}
+//         return i;
+//     }
+// }
 
 // TC : O(n)
 // SC : O(n)
 
-// Two Pointers Based Approach :
+
+// Optimised Approach :
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 1; // Represents the index where the next unique element needs to be placed.
+        if(nums.length == 0) return 0;
+
+        // Concept of slow and fast pointers
+        int i = 1; // The index at which the next unique element needs to be placed 
         int previousElement = nums[0];
 
         for(int j = 1; j < nums.length; j++) {
             if(nums[j] != previousElement) {
                 nums[i] = nums[j];
                 previousElement = nums[i];
-                i++; // Important setup for Slow and Fast Pointers 
+                i++;
             }
         }
 
-        return i;
+       return i;
     }
 }
 
 // TC : O(n)
 // SC : O(1)
-
