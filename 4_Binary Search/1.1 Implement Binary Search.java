cc31022838565e60c73d36db8bab69b59be332f1
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-15 12:40:49 +0530

    Create 1.1 Implement Binary Search.java

---
+++ 4_Binary Search/1.1 Implement Binary Search.java
@@ -0,0 +1,29 @@
+// Standard Binary Search Algorithm : https://leetcode.com/problems/binary-search/
+
+// Intuitive Approach : 
+// Linear Search - TC : O(n), SC : O(1)
+
+// Optimised Approach : 
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0; 
+        int high = nums.length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
+// TC : O(log n)
+// SC : O(1)
