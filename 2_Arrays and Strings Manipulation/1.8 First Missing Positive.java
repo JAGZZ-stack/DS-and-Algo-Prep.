28a38676fe3ef56fca07f7311644a95ba1ffd90e
JAGZZ-stack <jagshwaran20@gmail.com>
2026-01-15 23:05:06 +0530

    Update 1.8 First Missing Positive.java

--- 2_Arrays and Strings Manipulation/1.8 First Missing Positive.java
+++ 2_Arrays and Strings Manipulation/1.8 First Missing Positive.java
@@ -1,18 +1,40 @@
-// First Missing Positive : https://leetcode.com/problems/first-missing-positive/editorial/
+// First missing positive - https://leetcode.com/problems/first-missing-positive/submissions/1886073542/
+
+// Brute Force Approach : 
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        // Place all the valid elements in their correct index
+        for(int i = 1; i <= nums.length; i++) {
+            int val = i;
+            boolean isPresent = false;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] == val) {
+                    isPresent = true;
+                    break;
+                }
+            }
+            if(!isPresent) return val;
+        }
+
+        return 0;
+    }
+}
+
+// TC : O(n * n)
+// SC : O(1)
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        // Step 1 : Place all the valid elements of the array at the right index
         for(int i = 0; i < nums.length; i++) {
-            while(nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
-                int targetIdx = nums[i] - 1;
+            while(nums[i] >= 1 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
                 int temp = nums[i];
-                nums[i] = nums[targetIdx];
-                nums[targetIdx] = temp;
+                nums[i] = nums[nums[i] - 1];
+                nums[temp - 1] = temp; // Very important 
             }
         }
 
-        // Check for the first missing positive
+        // Step 2 : Check for the first mismatch
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != i + 1) {
                 return i + 1;
@@ -25,4 +47,3 @@ class Solution {
 
 // TC : O(n)
 // SC : O(1)
-
